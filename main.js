9c6004795fbb4f4e49ed790d2870c05037b33ab5
Nason Drake <39218689+MaskedRiderDecade@users.noreply.github.com>
2020-12-01 00:05:54 +0800

    learning event function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//只有view（视图）指向model（模型）的引用，没有反过来的 所以可能开发架构的核心类是以view为中心这个样子的
+//view到model是多对一的关系 两方都有对应的api，所以可能开发架构的核心类是以view为中心这个样子的
 //具体是指先获取到对应的diagram这个视图，然后遍历每个view看对应model的name是否正确
-//无法使用name直接搜索model的功能，因为不存在从model指向view的索引，只有反过来的
 
 function modifyElement () {
   var selectedViews = app.selections.getSelectedViews();
@@ -12,15 +11,20 @@ function modifyElement () {
 }
 
 function createModel () {
-  console.log(app.factory.getModelIds());
-  // Get a reference to top-level project
-  var project = app.repository.select("@Project")[0]
-  // Create a UMLModel element as a child of project
-  var model1 = app.factory.createModel({ id: "UMLModel", parent: project })
-  // Create a UMLClass element as a child of the model
-  var class1 = app.factory.createModel({ id: "UMLClass", parent: model1 })
-  // Create a UMLAttribute element and add to the field 'attributes' of the class
-  var attr1 = app.factory.createModel({ id: "UMLAttribute", parent: class1, field: "attributes" })
+  var diagram1=app.diagrams.getCurrentDiagram()
+  var options1 = {
+    id: "UMLClass",
+    parent: diagram1._parent,
+    diagram: diagram1,
+    x1: 100,
+    y1: 100,
+    x2: 200,
+    y2: 200,
+    modelInitializer: function (elem){
+      elem.name="Gundam"
+    }
+  }
+  var classView1 = app.factory.createModelAndView(options1)
 }
 
 function searchClassDiagram(name){
@@ -36,7 +40,8 @@ function searchClassDiagram(name){
 }
 
 function handleShowMessage(){
-  postSelectedView();
+  // postSelectedView();
+  createModel()
 }
 
 //图内选择一个class元素就是同时选择了view和model，一个view可能对应多个model
@@ -76,6 +81,47 @@ function postSelectedModel(){
 
 function init () {
   app.commands.register('helloworld:show-message', handleShowMessage)
+  app.factory.on('elementCreated',function(model,view){
+    console.log(view.model.name)
+    console.log(view.left)
+    console.log(view.right)
+    console.log(view.width)
+    console.log(view.height)
+  })
+  app.repository.on('updated',function(updatedElems){
+    var flag=false
+    for(i=0;i<updatedElems.length;i++){
+      if(updatedElems[i].getDisplayClassName()==='Class'){
+        name=updatedElems[i].name
+        console.log(name)
+        flag=true
+        break
+      }
+    }
+    if(!flag){
+      return
+    }
+    var diagram=app.diagrams.getCurrentDiagram()
+    var views=diagram.ownedViews
+    for(i=0;i<views.length;i++){
+      var view=views[i]
+      if(view.model.name===name){
+        var childArray=view.model.getChildren()
+        for(j=0;j<childArray.length;j++){
+          console.log(childArray[j].getDisplayClassName())
+          console.log(childArray[j].name)
+        }
+        console.log(view.model.name)
+        console.log(view.left)
+        console.log(view.right)
+        console.log(view.width)
+        console.log(view.height)
+      }
+    }
+
+  })
+
 }
 
 exports.init = init
+
