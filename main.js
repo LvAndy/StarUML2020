2f9b503491b05432165dd2028fca423af91bdee3
Nason Drake <39218689+MaskedRiderDecade@users.noreply.github.com>
2020-12-08 00:37:17 +0800

    add modify elements function

--- main.js
+++ main.js
@@ -1,18 +1,88 @@
 //view到model是多对一的关系 两方都有对应的api，所以可能开发架构的核心类是以view为中心这个样子的
 //具体是指先获取到对应的diagram这个视图，然后遍历每个view看对应model的name是否正确
 
-function modifyElement () {
-  var selectedViews = app.selections.getSelectedViews();
-  console.log(selectedViews[0].left);
-  console.log(selectedViews[0].right);
-  console.log(selectedViews[0].width);
-  console.log(selectedViews[0].height);
-  app.engine.setProperty(selectedViews[0], 'height',60);
+//修改名称
+function modifyClassName(newName){
+  var diagram=app.diagrams.getCurrentDiagram()
+  var views=diagram.ownedViews
+  for(i=0;i<views.length;i++){
+    var view=views[i]
+    if(view.model.name==="Class1"){
+      var model=view.model
+      app.engine.setProperty(model, 'name',newName);
+    }
+  }
 }
 
-function modifyClassModelChild(){
+function moveView(){
   var diagram=app.diagrams.getCurrentDiagram()
+  var views=diagram.ownedViews
+  for(i=0;i<views.length;i++){
+    var view=views[i]
+    if(view.model.name==="Class1"){
+      //moveViews接口中Editor类不明 无法使用，只能直接设定属性值
+      var width=view.width
+      var height=view.height
+      app.engine.setProperty(view,"left",view.left-10)
+      app.engine.setProperty(view,"top",view.top-10)
+      app.engine.setProperty(view,"width",width)
+      app.engine.setProperty(view,"height",height)
 
+    }
+  }
+}
+
+//移动视图
+function moveView(x,y){
+  var diagram=app.diagrams.getCurrentDiagram()
+  var views=diagram.ownedViews
+  for(i=0;i<views.length;i++){
+    var view=views[i]
+    if(view.model.name==="Class1"){
+      //moveViews接口中Editor类不明 无法使用，只能直接设定属性值
+      var width=view.width
+      var height=view.height
+      app.engine.setProperty(view,"left",x)
+      app.engine.setProperty(view,"top",y)
+      app.engine.setProperty(view,"width",width)
+      app.engine.setProperty(view,"height",height)
+
+
+    }
+  }
+}
+
+//调整视图大小
+function resizeView(){
+  var diagram=app.diagrams.getCurrentDiagram()
+  var views=diagram.ownedViews
+  for(i=0;i<views.length;i++){
+    var view=views[i]
+    if(view.model.name==="Class1"){
+      //resizeNode接口中Editor类不明 无法使用，只能直接设定属性值
+      app.engine.setProperty(view,"width",view.width+10)
+      app.engine.setProperty(view,"height",view.height+10)
+
+    }
+  }
+}
+
+//设置属性/接口名称
+function modifyClassModelAttribute(){
+  var diagram=app.diagrams.getCurrentDiagram()
+  var views=diagram.ownedViews
+  for(i=0;i<views.length;i++){
+    var view=views[i]
+    if(view.model.name==="Class1"){
+      var model=view.model
+      var children=model.getChildren()
+      children[0].name="age"
+      children[1].name="getAge"
+      //需要以下操作后才能生效，原因不明。。。
+      moveView(view.left-10,view.top)
+      moveView(view.left+10,view.top)
+    }
+  }
 }
 
 function createModel () {
@@ -32,24 +102,15 @@ function createModel () {
   var classView1 = app.factory.createModelAndView(options1)
 }
 
-function searchClassDiagram(name){
-  // Get a reference to top-level project
-  var parent=app.repository.select(name)[0]._parent;
-  var array=parent.ownedElements;
-  for(i=0;i<array.length;i++){
-    if(array[i].name===name){
-      console.log(array[i]);
-      break;
-    }
-  }
-}
-
+//目前默认全部修改的是“Class1”这个类，所有四个函数验证可用
 function handleShowMessage(){
-  // postSelectedView();
-  createModel()
+  modifyClassName('Student')
+  // moveView()
+  // resizeView()
+  // modifyClassModelAttribute()
 }
 
-//图内选择一个class元素就是同时选择了view和model，一个view可能对应多个model
+//图内选择一个class元素就是同时选择了view和model
 //选择一个列表里的class就只是选择了model
 function postSelectedView(){
   var xhr=new XMLHttpRequest();
@@ -64,23 +125,12 @@ function postSelectedView(){
     obj.diagramName=selectedView.getDiagram().name;
     obj.modelName=selectedView.model.name;
     obj.left=selectedView.left;
-    obj.right=selectedView.right;
+    obj.top=selectedView.top;
     obj.width=selectedView.width;
     obj.height=selectedView.height;
     resViews.push(obj);
   }
   var json=JSON.stringify(resViews);
-  xhr.send(json);
-}
-
-function postSelectedModel(){
-  var xhr=new XMLHttpRequest();
-  xhr.open('POST','http://localhost:80/api/postSelectedModel',true);
-  xhr.setRequestHeader('Content-type','application/json');
-  xhr.withCredentials=true;
-  var selectedModels = app.selections.getSelectedModels();
-  var modelData={'Models':selectedModels};
-  var json=JSON.stringify(modelData);
   xhr.send(json);
 }
 
@@ -90,11 +140,11 @@ function init () {
   app.factory.on('elementCreated',function(model,view){
     console.log(view.model.name)
     console.log(view.left)
-    console.log(view.right)
+    console.log(view.top)
     console.log(view.width)
     console.log(view.height)
   })
-  //修改类名称或者点击创建/删除属性或接口事件
+  //修改类名称或者点击创建/删除属性或接口按钮事件
   app.repository.on('updated',function(updatedElems){
     var flag=false
     for(i=0;i<updatedElems.length;i++){
@@ -106,6 +156,7 @@ function init () {
         break
       }
     }
+    //更新的element数组里没有类型为Class的元素，返回
     if(!flag){
       return
     }
@@ -121,7 +172,7 @@ function init () {
         }
         console.log(view.model.name)
         console.log(view.left)
-        console.log(view.right)
+        console.log(view.top)
         console.log(view.width)
         console.log(view.height)
       }
@@ -131,14 +182,14 @@ function init () {
   })
   //编辑属性或接口名称事件
   app.repository.on('operationExecuted',function(operation){
-    console.log(operation)
+    
     if(operation.name=='change properties'||operation.name=='change operation'){
       console.log(app.selections.getSelectedModels()[0]._parent.name)
       console.log(operation.ops[0].arg.n)
       console.log(operation.ops[0].arg.o)
     }
     if(operation.name=='move views'){
-
+      console.log(operation)
     }
     if(operation.name=='resize node'){
       
